1c89e2aee9b83b6c9801c526b8e327ec74e5bde2
Mark Cerritelli <echidnapi@hybrid-tomato.local>
2015-11-18 13:57:33 -0500

    Get some source material to use

--- generator.rb
+++ generator.rb
@@ -13,9 +13,16 @@ end
 novel << names.sample
 novel << " and it was good."
 
-time = Time.now.to_s
+time = Time.now.to_s.gsub(" ", "_")
 file_name = time + "_novel.txt"
 
 output = File.open( file_name,"w" )
 output << novel
 output.close
+
+
+
+# clean up text
+# content = File.readlines "/Users/echidnapi/Documents/nanogenmo-2015/sources/bible.txt"
+# content.each {|line| line.gsub! /\d+:\d+/i,""}
+# File.open('output.txt', 'w') { |f| content.each { |line| f << line } }
