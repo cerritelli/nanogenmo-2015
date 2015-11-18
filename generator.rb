cf7f73b2ea9ab721fb3c036200e3f781b1757105
Mark Cerritelli <echidnapi@hybrid-tomato.local>
2015-11-18 17:30:11 -0500

    Basic markov for creation

--- generator.rb
+++ generator.rb
@@ -1,5 +1,6 @@
 require 'open3'
 require 'pp'
+require "set"
 
 pp "Let's generator a very silly novel."
 
@@ -21,8 +22,33 @@ output << novel
 output.close
 
 
+file = File.open("/Users/echidnapi/Documents/nanogenmo-2015/sources/fixedbible.txt", "r")
+bible = file.read
+bible_words = bible.split
+
+dict = {}
+
+(bible_words.count - 3).times do |i|
+  prefix = bible_words[i..(i + 1)]
+  suffix = [bible_words[i + 2], bible_words[i + 3]]
+  dict[prefix] ||= Set.new
+  dict[prefix] << suffix
+end
+
+prefix = ["In", "the"]
+text = prefix.join " "
+
+20.times do 
+  suffix_set = dict[prefix]
+  suffix = suffix_set.to_a.sample
+  text << " "
+  text << suffix.to_a.join(" ")
+  prefix = suffix
+end
+pp text
 
 # clean up text
 # content = File.readlines "/Users/echidnapi/Documents/nanogenmo-2015/sources/bible.txt"
 # content.each {|line| line.gsub! /\d+:\d+/i,""}
 # File.open('output.txt', 'w') { |f| content.each { |line| f << line } }
+
