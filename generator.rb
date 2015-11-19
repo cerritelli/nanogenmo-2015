4ddaa787a740bb6bd60fb82a0fc6ed38803f00bc
Mark Cerritelli <echidnapi@hybrid-tomato.local>
2015-11-19 01:33:57 -0500

    short stories

--- generator.rb
+++ generator.rb
@@ -3,6 +3,7 @@ require 'pp'
 require "set"
 
 pp "Let's generator a very silly novel."
+puts "\n\n"
 
 names =  %w(Alex John Jason Laura Catherine Shawn Olivia William Mark Joseph Anthony Caroline Robert Aimee)
 novel = "And so it was that "
@@ -22,7 +23,7 @@ output << novel
 output.close
 
 
-file = File.open("/Users/echidnapi/Documents/nanogenmo-2015/sources/fixedbible.txt", "r")
+file = File.open("/Users/echidnapi/Documents/nanogenmo-2015/creation\ myths/long\ stories/genesis.txt", "r")
 bible = file.read
 bible_words = bible.split
 
@@ -38,7 +39,7 @@ end
 prefix = ["In", "the"]
 text = prefix.join " "
 
-20.times do 
+50.times do 
   suffix_set = dict[prefix]
   suffix = suffix_set.to_a.sample
   text << " "
@@ -46,6 +47,56 @@ text = prefix.join " "
   prefix = suffix
 end
 pp text
+
+puts "\n\n"
+creation_dict = {}
+Dir.glob('/Users/echidnapi/Documents/nanogenmo-2015/creation\ myths/short\ stories/*.txt') do |rb_file|
+  file = File.open(rb_file, "r")
+  story = file.read
+  story_words = story.split
+
+  (story_words.count - 3).times do |i|
+    prefix = story_words[i..(i + 1)]
+    suffix = [story_words[i + 2], story_words[i + 3]]
+    creation_dict[prefix] ||= Set.new
+    creation_dict[prefix] << suffix
+  end
+end
+
+prefix = ["In", "the"]
+text = prefix.join " "
+
+50.times do 
+  suffix_set = creation_dict[prefix]
+  suffix = suffix_set.to_a.sample
+  text << " "
+  text << suffix.to_a.join(" ")
+  prefix = suffix
+end
+pp text
+
+puts "\n\n"
+
+
+(bible_words.count - 3).times do |i|
+  prefix = bible_words[i..(i + 1)]
+  suffix = [bible_words[i + 2], bible_words[i + 3]]
+  creation_dict[prefix] ||= Set.new
+  creation_dict[prefix] << suffix
+end
+prefix = ["In", "the"]
+text = prefix.join " "
+
+50.times do 
+  suffix_set = creation_dict[prefix]
+  suffix = suffix_set.to_a.sample
+  text << " "
+  text << suffix.to_a.join(" ")
+  prefix = suffix
+end
+pp text
+
+
 
 # clean up text
 # content = File.readlines "/Users/echidnapi/Documents/nanogenmo-2015/sources/bible.txt"
